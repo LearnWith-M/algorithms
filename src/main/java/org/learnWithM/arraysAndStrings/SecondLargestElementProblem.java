5e76ab4b9f543ab72f137daedfc848ee15fc9d4e
moreenhettihewa <moreenmadhushani@gmail.com>
2025-09-16 22:41:47 +0530

    Arrays & Strings - Find Second Largest Element in an array ( Sol.2)

--- src/main/java/org/learnWithM/arraysAndStrings/SecondLargestElementProblem.java
+++ src/main/java/org/learnWithM/arraysAndStrings/SecondLargestElementProblem.java
@@ -25,9 +25,30 @@ public class SecondLargestElementProblem {
 
     }
 
+    //    Solution 2 : In one pass
+    public static void findSecondLargeElementOnePass(int[] arr) {
+        int arrLen = arr.length;
+
+        int largest = -1;
+        int secondLargest = -1;
+
+        for (int i = 0; i < arrLen - 1; i++) {
+            if (arr[i] > largest) {
+                secondLargest = largest;
+                largest = arr[i];
+            }
+            else if (arr[i] > secondLargest && arr[i] < largest) {
+                secondLargest = arr[i];
+            }
+        }
+
+        System.out.println("Second Largest element in one pass : " + secondLargest);
+    }
+
     public static void main(String[] args) {
-        int[] arr = new int[]{12,35,1,10,34,1};
+        int[] arr = new int[]{12, 35, 1, 10, 34, 1};
         findSecondLargeElement(arr);
+        findSecondLargeElementOnePass(arr);
     }
 
 }
